4628fcbb12f5deba0ebcc7f580749e53d62b2e7d
Mr. Hacker <ayushrockzzz@users.noreply.github.com>
2020-10-20 11:59:25 +0530

    first commit

---
+++ about.php
@@ -0,0 +1,109 @@
+
+<?php
+      ob_start();
+      session_start();
+      include"includes/db_conn.php";
+      include"includes/header.php";
+?>
+    <body>
+    <header id="home">
+<?php 
+      include"includes/navmenu.php";
+?>
+    </header>
+
+<br>
+
+
+
+<div class="container py-xl-12 py-lg-12">
+		<h3 class="title text-capitalize font-weight-light text-dark text-center mb-5">	
+			<span class="font-weight-bold">The Institute</span>
+		</h3>
+
+		<div class="row inner_sec_info pt-md-4">
+			<div class="col-lg-12">
+				<div class="single-left1">
+				    <div class="col-md-12">
+						
+						<h6 class="details-heading font-italic my-4">About Institute</h6>
+						<p>
+							Established in 2003, JIMS, Vasant Kunj, New Delhi, has emerged as one of the topmost colleges of the university in just over a decade. The college has been awarded the highest grade, A, by the Joint Assessment Committee of GGS Indraprastha University and the Govt. of NCT of Delhi and by the University Academic Audit Committee, and '+A', the highest grade, by the State Fee Regulatory Committee of the Govt. of NCT of Delhi for the last over 5 years. The institute has also been awarded 'A' Grade by the National Assessment and Accreditation Council, Bengaluru. The college has also been adjudged as the topmost college for media education by the Centre for Education Growth and Research (CEGR), New Delhi, in 2015, as the 5th Best College in India for IT education by Global Human Resource Development Centre (GHRDC), New Delhi, in 2018 and as the 26th Best BBA B-School in the country in Times BBA B-School Survey 2018. Among the initiatives taken, the institute has become a Participating Member of UNGC &amp; UMPRME, New York, apart from establishing Institution Innovation Council under MHRD, Govt. of India, working for the uplift of five villages in NCT of Delhi under Unnat Bharat Abhiyan (UBA) scheme of the MHRD Govt. of India and broadcasting DST Sponsored Radio Mathematics programmes on its Community Radio 90.4 MHz.
+						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="computer-labs"></a>
+						<h6 class="details-heading font-italic my-4">Computer Labs</h6>
+						<p>
+							Institute provides state-of-art computing facilities to its students through its well-equipped Labs. The Labs have clusters based on Windows XP, Windows 2000 and Windows NT Servers, configured with Software such as C, C++, Visual Studio 6.0, .Net, RHEL5.0, JDK 1.3 and Oracle 11i. We also have many types of software for Mass Communication and Fashion Designing such as Adobe CS4 Master Collection with Indesign and Photoshop, Quark Express, PageMaker, Flash, Corel Draw, Corel Painter. The Internet content security at JIMS is provided by latest Firewall Watch Guard which has policies implemented for various website and download filtering. The entire network of JIMS is further protected by Trend Micro Antivirus a full featured and well equipped program that is geared towards keeping network free of Viruses and spy wares. The department has fully functioning labs covering: -  RDBMS Computer Organization Networking Multi-media Hardware Software Engineering LINUX - RHEL 5.0 .NET Oracle 11g Adobe Master Collection Quark Express MSDN iMac with FCP  The Institute has over 180 Pentium (P-IV 1.9/2.4/3.0/Core 2 Duo/Dual Core Processor) PC desktop computers and Notepad for Faculty members, which are fully networked, Wi-Fi enabled campus and have the latest software as prescribed by GGSIPU. The computer labs are air-conditioned, with adequate UPS backup, and have a 24 hours 'Internet Lease Line' facility through a 2 Mbps Internet connection.  Conference Hall						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="library"></a>
+						<h6 class="details-heading font-italic my-4">Library</h6>
+						<p>
+							institute has a well-stocked library with 8466 books including 3035 titles. It subscribes to more than 75 journals/magazine (Indian &amp; foreign) and 15 news papers. The library also has multiple copies of much sought after books and current and earlier issues of important journals/magazine. Recently it has started Book Bank and E-Journal services.<br>
+<br>
+<strong>Services and Facilities </strong><br>
+<br>
+<strong>Online Public Access Catalogue (OPAC)</strong>: OPAC helps the user to retrieve the reading material by the author, title, subject, classified number &amp; publisher. The OPAC also provides information about the status of the books available in the Library. <br>
+<br>
+<strong>Encyclopedia Britannica</strong>: Encyclopedia Britannica is installed in separate user terminal for student's reference.<br>
+<br>
+<strong>DELNET</strong>: DELNET has been established with the prime objective of promoting resource sharing among libraries through the development of a network of Libraries. DELNET at present has 1009 Libraries as its members of which 167 Libraries are in Delhi, 828 outside Delhi in 30 States and Union territories and 14 in oveseas countries.  The JIMS Library is a member of DELNET. As such, the users at JIMS Library can access databases hosted by DELNET. <br>
+<br>
+<strong>British Council Library Membership</strong>: The JIMS Library is a member of British Council Library. As such, the users of JIMS Library can also access database of British Council Library. Inter Campus Library Loan &amp; Resource sharing facilities: The Library arranges to procure Books and Journals available at other Libraries of JIMS on Inter Campus Library Loan for a limited period. <br>
+<br>
+<strong>Selective Dissemination of Information (SDI)</strong>: SDI is displayed on Notice boards for user's reference on weekly basis which give subject wise content information of latest Magazines/ Journals available in the library. <br>
+<br>
+<strong>Online Resources</strong>: The Library has also subscribed e-Journals related to Management, Mass Communication, Journalism, Information Technology etc. Users of JIMS library can access these e-Journals in the user's terminals in the Library. <br>
+<br>
+<strong>Online Library</strong>: The Institute also started Online library facility for students and faculty. Registered members can search for a particular book and also can read or download ebooks, ejouirnals, audio/video lectures.						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="community-radio"></a>
+						<h6 class="details-heading font-italic my-4">Community Radio</h6>
+						<p>
+							JIMS Campus Radio Community carry a variety of programs (relating to campus) including news, sports, discussion, event announcements as well as general music. Often the format is best described as a freeform radio format, with a lot of creativity and individualism among the disc jockeys and show hosts.						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="mass-comm-studio"></a>
+						<h6 class="details-heading font-italic my-4">Mass Comm. Studio</h6>
+						<p>
+							An Audio-Visual studio has been established for various purposes of shooting i.e.- Live, anchoring, Panel discussion, Interview, Group discussion, etc. Editing room is equipped with thelatest Non Linear editing system (E-Mac) . Students are learning NLE on Final cut pro pinnacle software .In studio latest 3CCD professional grade video camera is used for different shoots. Separate Audio studios have been developed for radio program production. Audio studio is fully equipped with digital audio workstation (DAW) and also supported by integrated DAW’s system. JIMS has been running a community radio station successfully						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="cafeteria"></a>
+						<h6 class="details-heading font-italic my-4">Cafeteria</h6>
+						<p>
+							The college cafeteria plays an important role in promoting healthy foods and creating a college culture of healthy eating. It caters to the students taste-bud and is a great place to unwind in between the hectic lecture-lab schedules. The cafeteria provides good quality and hygienic food at reasonable prices. There are multi cuisine varieties of food and beverages available for the students.						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="smart-classrooms"></a>
+						<h6 class="details-heading font-italic my-4">Smart Classrooms</h6>
+						<p>
+							&nbsp;All the classrooms are equipped with LCD projectors and computer systems. All classrooms are well furnished and fully air-conditioned.						</p>
+					</div>
+										<div class="col-md-12">
+						<a name="wifi-campus"></a>
+						<h6 class="details-heading font-italic my-4">WiFi Campus</h6>
+						<p>
+							</p><p style="font-family: Verdana, Arial, Helvetica, sans-serif;">The entire campus is Wi-Fi enabled with a high speed internet connection to allow the students and faculty to access the internet. The coverage of Wi-Fi is not only restricted to class rooms but also extends to all the areas including library, cafeteria etc. The campus is also very well connected with Bus routes and Metro Rail connectivity.</p>
+						<p></p>
+					</div>
+										<div class="col-md-12">
+						<a name="conference-hall"></a>
+						<h6 class="details-heading font-italic my-4">Conference Hall</h6>
+						<p>
+							&nbsp;The conference hall is an ultra-modern and well equipped with state-of-the-art audio-visual and presentation tools to facilitate smooth presentations. The air conditioned Conference Hall has a seating capacity of 30 people.						</p>
+					</div>
+									</div>
+			</div>
+			</div>
+<?php
+    include"includes/footer.php";
+    include"includes/modals.php";
+?>
+ 
+
+
+
---
+++ includes/carousel.php
@@ -0,0 +1,32 @@
+<!-- Sider Start -->
+<br>
+
+			<div class="slider">
+				<div id="fawesome-carousel" class="carousel slide" data-ride="carousel">
+					<ol class="carousel-indicators indicatior2">
+						<li data-target="#fawesome-carousel" data-slide-to="0" class="active"></li>
+						<li data-target="#fawesome-carousel" data-slide-to="1"></li>
+					</ol>
+				 
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<img src="https://scontent.fpat2-1.fna.fbcdn.net/v/t1.0-9/10342472_637680162976041_2456296268242028127_n.jpg?_nc_cat=107&_nc_sid=e3f864&_nc_ohc=_CUStwAxO-QAX_kRS4Q&_nc_ht=scontent.fpat2-1.fna&oh=41291a42eac54b3d4eb934260b0a4643&oe=5F92A654" alt="Sider Big Image" style = "width:100%">
+							<div class="carousel-caption">
+								<h1 class="wow fadeInLeft">Welcome To the Official Alumni network of Jagannath International Management School (JIMS) Vasant Kunj</h1>
+								<div class="slider-btn wow fadeIn">
+							
+								</div>
+							</div>
+						</div>
+						<div class="item">
+						<img src="https://scontent.fpat2-1.fna.fbcdn.net/v/t1.0-9/10342472_637680162976041_2456296268242028127_n.jpg?_nc_cat=107&_nc_sid=e3f864&_nc_ohc=_CUStwAxO-QAX_kRS4Q&_nc_ht=scontent.fpat2-1.fna&oh=41291a42eac54b3d4eb934260b0a4643&oe=5F92A654" alt="Sider Big Image" style = "width:100%">
+							<div class="carousel-caption">
+								<h1 class="wow fadeInLeft">Welcome To the Official Alumni network of Jagannath International Management School (JIMS) Vasant Kunj</h1>
+								<div class="slider-btn wow fadeIn">
+									
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<!-- Sider End -->
